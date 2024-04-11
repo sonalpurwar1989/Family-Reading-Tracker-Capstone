694c9d278b1ca25f6bd6d94c23c13b78eda051c9
Jamesson Koleno <Jamessonkoleno@gmail.com>
2024-04-11 12:31:15 -0400

    added photos to prizes

--- vue/src/components/PrizeView.vue
+++ vue/src/components/PrizeView.vue
@@ -21,11 +21,11 @@
   data(){
     return {
       prizes: [
-        { name: "Ice cream cone (5 hours)", image: "placeholder" },
-        { name: "Vinyl sticker (5 hours)", image: "placeholder" },
-        { name: "Journal (10 hours)", image: "placeholder" },
+        { name: "Ice cream cone (5 hours)", image: "src/assets/Images/FluffCone_HERO_071422_071422_37221.jpeg" },
+        { name: "Vinyl sticker (5 hours)", image: "src/assets/Images/81Tz4mgmZlL._UF894,1000_QL80_.jpg" },
+        { name: "Journal (10 hours)", image: "src/assets/Images/What-is-journal-writing-image-of-blank-journal.jpg" },
         { name: "Watercolor Paints (10 hours)", image: "placeholder" },
-        { name: "Kite (15 hours)", image: "placeholder" },
+        { name: "Kite (15 hours)", image: "src/assets/Images/International-Kite-Day-1200x834.jpg" },
         { name: "Puzzle (15 hours)", image: "placeholder" },
         { name: "Lego Set (20 hours)", image: "placeholder" },
         { name: "Book Store Gift Card (20 hours)", image: "placeholder" }
