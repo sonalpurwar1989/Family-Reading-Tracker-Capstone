03743e69b03f7358809424483ceb38424ee6ed29
Ronald Roberts <rbroberts0523@gmail.com>
2024-04-13 17:14:57 -0400

    bookservie frint end

--- vue/src/services/BookService.js
+++ vue/src/services/BookService.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
-
+const API_BASE_URL ='/';
 export default {
 
     getAllBooks(){
-     return axios.get('/book');
+     return axios.get(`${API_BASE_URL}/book`);
+    },
+    searchBooksByTitle(title){
+        return axios.get(`${API_BASE_URL}books/search`, {params: {title}});
     }
 
 }
