d88597dabd32f2ebb693ac2ed16013fc4d1ee86a
Ronald Roberts <rbroberts0523@gmail.com>
2024-04-17 14:28:17 -0400

    book service

---
+++ java/src/main/java/com/techelevator/services/PrizeService.java
@@ -0,0 +1,36 @@
+package com.techelevator.services;
+
+import com.techelevator.dao.PrizeDao;
+import com.techelevator.model.Prize;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class PrizeService {
+    private final PrizeDao prizeDao;
+
+    @Autowired
+    public PrizeService(PrizeDao prizeDao) {
+        this.prizeDao = prizeDao;
+    }
+
+    public List<Prize> getAllPrizes() {
+        return prizeDao.findAll();
+    }
+
+    public void createPrize(Prize prize) {
+        prizeDao.save(prize);
+    }
+
+    public Prize getPrizeById(Integer id) {
+        return prizeDao.findById(id);
+    }
+    public void updatePrize(Integer prizeId, Prize prize){
+        prize.setId(prizeId);
+        prizeDao.update(prize);
+    }
+    public void deletePrize(Integer prizeId){
+        prizeDao.deleteById(prizeId);
+    }
+}
+
